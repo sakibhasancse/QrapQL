e1ca78cea446bbd773a52652cfb960bbc8d30d8d
SakibHasanCse <sakibhasan9641@gmail.com>
2021-07-09 22:30:25 +0600

    like post

--- GraphQL/Src/service/likes.js
+++ GraphQL/Src/service/likes.js
@@ -3,6 +3,13 @@ const { checkAuth } = require('../../utils/auth')
 const { AuthorizationError, UserInputError } = require('apollo-server');
 module.exports = {
     async LikePost(_, { postID }, context) {
+        try {
+            const post = await Post.findById(postID);
+            if (!post) throw new Error('Post not found');
+
+        } catch (error) {
+
+        }
 
     }
 
